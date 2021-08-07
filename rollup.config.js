2ee2af3dddb5d91652ec498d33af9c916569a428
Francis Gulotta <wizard@roborooter.com>
2021-08-06 22:59:23 -0400

    fix: deps management
    - Move some stuff to  peerdeps.
    - bring in the graphql-ws stuff instead of using it as a dep

--- rollup.config.js
+++ rollup.config.js
@@ -9,5 +9,18 @@ export default {
     { format: 'esm', file: './dist/index-esm.mjs' },
     { format: 'cjs', file: './dist/index.js' },
   ],
-  external: ['aws-sdk', '@aws/dynamodb-data-mapper', '@aws/dynamodb-data-mapper-annotations', '@aws/dynamodb-expressions', 'graphql', 'graphql-ws', 'graphql/execution/execute', 'tslib'],
+  external: [
+    // peer deps
+    'aws-sdk',
+    'graphql',
+    'graphql/execution/execute',
+    // actual deps
+    '@aws/dynamodb-data-mapper',
+    '@aws/dynamodb-data-mapper-annotations',
+    '@aws/dynamodb-expressions',
+    // dep from ts while we're using these decorator stuff, want to include it
+    // 'tslib',
+    // we only use a string and types from this package so lets import it
+    // 'graphql-ws',
+  ],
 }
