8c7dc70270b5ce0f4f0abc579313e273b208be07
Francis Gulotta <wizard@roborooter.com>
2021-08-10 11:01:29 -0400

    chore: add some explanations in a test so I can show it to others

--- lib/tests/basic-events-test.ts
+++ lib/tests/basic-events-test.ts
@@ -1,6 +1,6 @@
 import { assert } from 'chai'
 import fetch from 'node-fetch'
-import { start as sandBoxStart, end as sandBoxEnd } from '@architect/sandbox'
+import { start as sandBoxStart, end as sandBoxStop } from '@architect/sandbox'
 import { createClient } from 'graphql-ws'
 import WebSocket from 'ws'
 import { deferGenerator } from 'inside-out-async'
@@ -11,7 +11,7 @@ before(async () => {
 })
 
 after(async () => {
-  await sandBoxEnd()
+  await sandBoxStop()
 })
 
 const pollURl = (url: string, timeout: number) => {
@@ -81,7 +81,14 @@ describe('Basic Events', () => {
 
   it('subscribes', async () => {
     const { values, unsubscribe } = await executeSubscription('subscription { greetings }')
+
+    // I need this because I don't know when the subscription actually occurs server side.
+    // This requests sends a bunch of data and completes the subscription
+    // The better way is to be able to send a websocket event back to the client confirming the subscription
+    // this isn't part of the graphql-ws spec, but an onSubscribe data event would be a good feature to have
+    // and could be abused for this purpose
     const cancelFetch = pollURl('http://localhost:3339/', 1000)
+
     const greetings = await collect(map((value: { greetings: string }) => value.greetings, values))
     assert.deepEqual(greetings, ['hi', 'hey!'])
     cancelFetch()
