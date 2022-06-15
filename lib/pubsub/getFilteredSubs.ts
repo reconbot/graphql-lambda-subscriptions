066f1de579eb434d924f943a1621b0e4f84b82ae
Michel Albers <m@MacBook-Pro-von-Michel.local>
2022-06-15 10:38:25 +0200

    chore: remove white space changes

--- lib/pubsub/getFilteredSubs.ts
+++ lib/pubsub/getFilteredSubs.ts
@@ -1,102 +1,102 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { collect } from "streaming-iterables";
-import { ServerClosure, Subscription } from "../types";
+import { collect } from 'streaming-iterables'
+import { ServerClosure, Subscription } from '../types'
 
 export const getFilteredSubs = async ({
   server,
   event,
 }: {
-  server: Omit<ServerClosure, "gateway">;
-  event: { topic: string; payload?: Record<string, any> };
+  server: Omit<ServerClosure, 'gateway'>
+  event: { topic: string, payload?: Record<string, any> }
 }): Promise<Subscription[]> => {
   if (!event.payload || Object.keys(event.payload).length === 0) {
-    server.log("getFilteredSubs", { event });
+    server.log('getFilteredSubs', { event })
 
     const iterator = server.models.subscription.query({
-      IndexName: "TopicIndex",
-      ExpressionAttributeNames: { "#a": "topic" },
-      ExpressionAttributeValues: { ":1": event.topic },
-      KeyConditionExpression: "#a = :1",
-    });
+      IndexName: 'TopicIndex',
+      ExpressionAttributeNames: { '#a': 'topic' },
+      ExpressionAttributeValues: { ':1': event.topic },
+      KeyConditionExpression: '#a = :1',
+    })
 
-    return await collect(iterator);
+    return await collect(iterator)
   }
-  const flattenPayload = collapseKeys(event.payload);
+  const flattenPayload = collapseKeys(event.payload)
 
-  const filterExpressions: string[] = [];
+  const filterExpressions: string[] = []
   const expressionAttributeValues: {
-    [key: string]: string | number | boolean;
-  } = {};
-  const expressionAttributeNames: { [key: string]: string } = {};
+    [key: string]: string | number | boolean
+  } = {}
+  const expressionAttributeNames: { [key: string]: string } = {}
 
-  let attributeCounter = 0;
+  let attributeCounter = 0
   for (const [key, value] of Object.entries(flattenPayload)) {
-    let aliasNumber = attributeCounter++;
-    const keyParts = key.split(".");
+    const aliasNumber = attributeCounter++
+    const keyParts = key.split('.')
     const keyPartsAttributeName = keyParts
       .map((part, index) => `#${aliasNumber + index}`)
-      .join(".");
-    key.split(".").forEach((keyPart, index) => {
-      expressionAttributeNames[`#${aliasNumber + index}`] = keyPart;
-      attributeCounter += index;
-    });
-    expressionAttributeValues[`:${aliasNumber}`] = value;
+      .join('.')
+    key.split('.').forEach((keyPart, index) => {
+      expressionAttributeNames[`#${aliasNumber + index}`] = keyPart
+      attributeCounter += index
+    })
+    expressionAttributeValues[`:${aliasNumber}`] = value
 
     filterExpressions.push(
-      `(#filter.${keyPartsAttributeName} = :${aliasNumber} OR attribute_not_exists(#filter.${keyPartsAttributeName}))`
-    );
+      `(#filter.${keyPartsAttributeName} = :${aliasNumber} OR attribute_not_exists(#filter.${keyPartsAttributeName}))`,
+    )
   }
 
-  server.log("getFilteredSubs", {
+  server.log('getFilteredSubs', {
     event,
     expressionAttributeNames,
     expressionAttributeValues,
     filterExpressions,
-  });
+  })
 
   const iterator = server.models.subscription.query({
-    IndexName: "TopicIndex",
+    IndexName: 'TopicIndex',
     ExpressionAttributeNames: {
-      "#hashKey": "topic",
-      "#filter": "filter",
+      '#hashKey': 'topic',
+      '#filter': 'filter',
       ...expressionAttributeNames,
     },
     ExpressionAttributeValues: {
-      ":hashKey": event.topic,
+      ':hashKey': event.topic,
       ...expressionAttributeValues,
     },
-    KeyConditionExpression: "#hashKey = :hashKey",
-    FilterExpression: filterExpressions.join(" AND ") || undefined,
-  });
+    KeyConditionExpression: '#hashKey = :hashKey',
+    FilterExpression: filterExpressions.join(' AND ') || undefined,
+  })
 
-  return await collect(iterator);
-};
+  return await collect(iterator)
+}
 
 export const collapseKeys = (
-  obj: Record<string, any>
+  obj: Record<string, any>,
 ): Record<string, number | string | boolean> => {
-  const record = {};
+  const record = {}
   for (const [k1, v1] of Object.entries(obj)) {
     if (
-      typeof v1 === "string" ||
-      typeof v1 === "number" ||
-      typeof v1 === "boolean"
+      typeof v1 === 'string' ||
+      typeof v1 === 'number' ||
+      typeof v1 === 'boolean'
     ) {
-      record[k1] = v1;
-      continue;
+      record[k1] = v1
+      continue
     }
 
-    if (v1 && typeof v1 === "object") {
-      const next = {};
+    if (v1 && typeof v1 === 'object') {
+      const next = {}
 
       for (const [k2, v2] of Object.entries(v1)) {
-        next[`${k1}.${k2}`] = v2;
+        next[`${k1}.${k2}`] = v2
       }
 
       for (const [k1, v1] of Object.entries(collapseKeys(next))) {
-        record[k1] = v1;
+        record[k1] = v1
       }
     }
   }
-  return record;
-};
+  return record
+}
